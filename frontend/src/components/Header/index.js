32967e66cdb1d3bacdf24af32b989b734b3d0e1c
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-21 15:35:15 -0300

    Typography

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -3,14 +3,14 @@ import styled from './style.module.css';
 const Header = () => {
     return (
         <header className={styled.header}>
-            <h1>Preenchimento e Visualização de Atividades</h1>
+            <h1 className="title1">Preenchimento e Visualização de Atividades</h1>
 
             <nav>
-                <label>Visualização</label>
+                <label className="navegation-text">Visualização</label>
             </nav>  
 
             <div className={styled.user}>
-                <label>Nome do usuário</label>
+                <label className="label">Nome do usuário</label>
             </div>
         </header>
     )
