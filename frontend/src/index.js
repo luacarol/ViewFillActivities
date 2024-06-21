7abd98c14d76d1c7f68eef06cd26be3018fa47f2
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-21 16:28:08 -0300

    react router config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import NewActivities from './pages/NewActivities';
-import Header from './components/Header';
+import Header from './components/Header/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header/>
-    <NewActivities/>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<NewActivities />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
