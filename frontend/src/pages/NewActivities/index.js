f2701a0b685e98186e59687b05a9b34adcf9c703
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-21 14:40:38 -0300

    styled components and new activities page

---
+++ frontend/src/pages/NewActivities/index.js
@@ -0,0 +1,11 @@
+import styles from './style.module.css';
+
+const NewActivities = () => {
+    return (
+        <div className={styles.container}>
+            <h1>NewActivities</h1>
+        </div>
+    )
+}
+
+export default NewActivities;
